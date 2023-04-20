979ebc476b41aefc5e960e21c81a88c54c7c2bdf
Carlos Arturo Torres <carlosat95@gmail.com>
2023-04-20 08:49:34 -0500

    vite + reac

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let title = 'WELCOME TO MINDHUM'
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>{title.toLowerCase()}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+
+/* 
+PascalCase - Ejemplos:
+Idex
+Button
+Card
+Carousel
+FormRegister
+formRegister //la sintaxis de camelCase na va para definir componentes
+ */
