a1c66ca5cea23b91b22c56b6eaac1e48d78a20d6
CATorres Varon <44732360+ctorresv@users.noreply.github.com>
2023-04-26 08:40:50 -0500

    F-04, fix carrusel, fix footer (#11)

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,16 +1,36 @@
+import { useEffect, useState } from "react"
 import logo from '../images/Logo.png'
 
 export default function Navbar() {
+
+  let [navOpen, setNavOpen] = useState(false)
+
+  let toggleNav = () => {
+    setNavOpen(!navOpen)
+  }
+
+  useEffect(() => {
+    console.log(navOpen);
+  }, [navOpen])
+
   return (
     <header >
-        <nav className="flex justify-between items-center p-4">
-            <a href='#' className="flex w-[57px] h-[55px] flex-col justify-center gap-[2px] px-3">
-                <div className="w-[26.95px] border-[2px] border-white rounded-sm"></div>
-                <div className="w-[26.95px] border-[2px] border-white rounded-sm"></div>
-                <div className="w-[26.95px] border-[2px] border-white rounded-sm"></div>
-            </a>
-            <img src={logo} alt="" />
-        </nav>
+      <nav className="flex justify-between items-center p-4 flex-wrap">
+        <button onClick={toggleNav} className="flex w-[57px] h-[55px] flex-col justify-center gap-[2px] px-3 my-1 cel:order-1">
+          <div className="w-[26.95px] border-[2px] border-white rounded-sm"></div>
+          <div className="w-[26.95px] border-[2px] border-white rounded-sm"></div>
+          <div className="w-[26.95px] border-[2px] border-white rounded-sm"></div>
+        </button>
+        {navOpen ? 
+          <div className=" text-white flex-wrap mt-3 cel:order-3 sm:order-2">
+            <a className=" p-2 border-[2px] rounded-xl mx-1" href="#">Home</a>
+            <a className=" p-2 border-[2px] rounded-xl mx-1" href="#">Mangas</a>
+            <a className=" p-2 border-[2px] rounded-xl mx-1" href="#">My Mangas</a>
+          </div>
+          : <div className="cel:order-3 sm:order-2" ></div>}
+
+        <img className="my-1 cel:order-2 sm:order-3" src={logo} alt="" />
+      </nav>
     </header>
   )
 }
