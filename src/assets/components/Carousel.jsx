eef5ffc1070275736e261158c2e2cb6aeac630d7
Carlos Arturo Torres <carlosat95@gmail.com>
2023-04-26 08:39:19 -0500

    F-04, fix carrusel, fix footer

--- src/assets/components/Carousel.jsx
+++ src/assets/components/Carousel.jsx
@@ -3,20 +3,29 @@ import axios from "axios"
 import apiUrl from '../../../api.js'
 
 export default function Carousel() {
+  const [images, setImgs] = useState([])
+  const [counter, setCounter] = useState(0)
     
   useEffect(() => {
-    axios(apiUrl + 'carousels')
-      .then(res => setImgs(res.data.resources))
+    console.log('entramos al efecto')
+    const result = axios(apiUrl+'carousels')
+      .then(res => {
+        console.log('este es el contenido de images',images);
+        console.log('respuesta api ',res,res.data.resources);
+        setImgs(res.data.resources)        
+      })
       .catch(err => console.log(err))
-
-    const interval = setInterval(changeImage, 4000);
-    return () => clearInterval(interval)},
+    },
     []
   )
 
-  const [images, setImgs] = useState([])
-  const [counter, setCounter] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(changeImage, 4000)
+    console.log('contenido de images:', images)
 
+    return () => clearInterval(interval)     
+  }, [images])
+  
   // cont = undefine
   const changeImage = () => {
     setCounter(cont => {
@@ -25,20 +34,20 @@ export default function Carousel() {
       //segunda iteracion cont = 1 (cont = 1 es menor que images.length = 2)
       //tercera iteracion cont = 2 (cont = 2 no es menor que images.length = 2)
       if (cont < images.length - 1) {
-        return cont += 1
         //primera iteracion cont = 1
+        return cont += 1
         //segunda iteracion cont = 2
       } else {
         return cont = 0
         //tercera iteracion cont = 0
-      }
+      } 
     })
     
   }
     return (
       <>
-        <div className='ml-[2rem] pt-[3rem] w-full xl:max-w-[639px] lg:max-w-[550px] h-auto p-3 mt-10 '>
-          <img  src={images[counter]?.cover_photo} alt="img" />
+        <div className='ml-[2rem] pt-[3rem]  mt-10 '>
+          <img className="w-full xl:max-w-[639px] lg:max-w-[550px] h-auto p-3" src={images[counter]?.cover_photo} alt="img" />
         </div> 
       </>
   )
--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -8,12 +8,12 @@ import logo from '../images/Logo.png'
 
 export default function Footer() {
   return (
-    <footer className='h-[80vh] bg-white w-full'>
+    <footer className='h-[80vh] bg-black w-full'>
           <div>
             <img className='h-[336px] rounded-b-[65%] w-full object-cover' src={imgFooter} alt="" />
           </div>
           <div className='flex justify-evenly items-center h-[323px] flex-wrap'>
-            <div className='flex w-[180px] justify-around'>
+            <div className='flex w-[180px] justify-around text-white'>
               <p>Home</p>
               <p>Mangas</p>
             </div>
@@ -26,7 +26,7 @@ export default function Footer() {
                 <img src={yt} alt="" />
               </div>
               <div>
-                <h4 className='rounded-[4px] w-[221px] h-[43px] bg-gradient-to-r from-[#434343] to-[#000000] text-white flex items-center justify-center'>                 
+                <h4 className='rounded-[4px] w-[221px] h-[43px] bg-white text-black flex items-center justify-center'>                 
                   Donate
                   <img className='p-2' src={corazon} alt="" /></h4>
               </div>
